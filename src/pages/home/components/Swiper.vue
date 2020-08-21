65de57fecca5dc423f823f0c52d01bf8b0969dbf
ZHOUWEI <361772818@qq.com>
2020-08-21 10:44:22 +0800

    swiper init

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img 
+          class="swiper-img"
+          :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.wrapper /deep/ .swiper-pagination-bullet-active {
+  background-color: #fff !important;
+}
+.wrapper {
+  width: 100%;
+  height: 0; //相较于父级元素高度
+  overflow: hidden;
+  padding-bottom: 31.25%; //相较于宽度
+  background-color: #eee;
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>

bd75bf1dd09817385ee7930d31cef4c7f168a02f
ZHOUWEI <361772818@qq.com>
2020-08-24 13:26:46 +0800

    add cityy header

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cityHeader'
+}
+</script>
+
+<style scoped lang="less">
+@import url("~@/assets/styles/varibles.less");
+.header {
+  position: relative;
+  overflow: hidden;
+  height: @headerHeight;
+  line-height: @headerHeight;
+  text-align: center;
+  color: #fff;
+  background-color: @bgColor;
+  .header-back {
+    position: absolute;
+    width: 0.64rem;
+    text-align: center;
+    font-size: 0.4rem;
+    top: 0;
+    color: #fff;
+  }
+}
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="header">
-        <div class="header-left">
-          <span class="iconfont back-icon">&#xe624;</span>
-        </div>
-        <div class="header-input">
-          <span class="iconfont search-icon">&#xe632;</span>
-          输入城市/景点
-        </div>
-        <div class="header-right">
-          {{this.city}}
-          <span class="iconfont">&#xe64a;</span>
-        </div>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span>
     </div>
+    <div class="header-input">
+      <span class="iconfont search-icon">&#xe632;</span>
+      输入城市/景点
+    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont">&#xe64a;</span>
+      </div>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -24,37 +26,38 @@ export default {
 </script>
 
 <style scoped lang="less">
-@import url('~@/assets/styles/varibles.less');
+@import url("~@/assets/styles/varibles.less");
 .header {
-    display: flex;
-    line-height: .86rem;
-    background-color: @bgColor;
+  display: flex;
+  line-height: @headerHeight;
+  background-color: @bgColor;
+  color: #fff;
+  .header-left {
+    width: 0.64rem;
+    float: left;
+    text-align: center;
+    .back-icon {
+      font-size: 0.4rem;
+    }
+  }
+  .header-input {
+    flex: 1;
+    line-height: 0.64rem;
+    height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    background-color: #fff;
+    border-radius: 0.1rem;
+    color: #ccc;
+    .search-icon {
+      margin-left: 0.1rem;
+    }
+  }
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
     color: #fff;
-    .header-left {
-        width: 0.64rem;
-        float: left;
-        text-align: center;
-        .back-icon {
-          font-size: .4rem;
-        }
-    }
-    .header-input {
-        flex: 1;
-        line-height: .64rem;
-        height: .64rem;
-        margin-top: .12rem;
-        margin-left: .2rem;
-        background-color: #fff;
-        border-radius: .1rem;
-        color: #ccc;
-        .search-icon {
-          margin-left: .1rem;
-        }
-    }
-    .header-right {
-        width: 1.24rem;
-        float: right;
-        text-align: center;
-    }
+  }
 }
 </style>

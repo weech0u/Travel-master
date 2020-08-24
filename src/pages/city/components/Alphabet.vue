32adb3223e602bc8acea856e2294c940800f4d2f
ZHOUWEI <361772818@qq.com>
2020-08-24 14:23:26 +0800

    add list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'cityAlphabet'
+}
+</script>
+
+<style scoped lang="less">
+@import url("~@/assets/styles/varibles.less");
+.list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  top: 1.58rem;
+  right: 0;
+  bottom: 0;
+  width: .4rem;
+  .item {
+    line-height: .4rem;
+    color: @bgColor;
+  }
+}
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="list">
+    <div class="list-wrapper" ref="wrapper">
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">B</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">B</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">B</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+        <div class="item-list border-bottom">阿拉尔</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'cityList',
+  mounterd () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style scoped lang="less">
+@import url("~@/assets/styles/varibles.less");
+.border-topbottom {
+  &:before {
+    border-color: #ccc;
+  }
+  &:after {
+    border-color: #ccc;
+  }
+}
+.list {
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  .title {
+    line-height: 0.54rem;
+    background-color: #eee;
+    padding-left: 0.2rem;
+    color: #666;
+  }
+  .button-list {
+    padding: 0.1rem;
+    overflow: hidden;
+    padding: 0.1rem 0.6rem 0.1rem 0.1rem;
+    .button-wrapper {
+      float: left;
+      width: 33.33%;
+      .button {
+        margin: 0.1rem;
+        padding: 0.1rem 0;
+        text-align: center;
+        border: 0.02rem solid #ccc;
+        border-radius: 3px;
+      }
+    }
+  }
+  .item-list {
+    line-height: 0.54rem;
+    color: #666;
+    padding-left: 0.2rem;
+    .border-bottom {
+      &:before {
+        border-color: #ccc;
+      }
+      &:after {
+        border-color: #ccc;
+      }
+    }
+  }
+}
+</style>
